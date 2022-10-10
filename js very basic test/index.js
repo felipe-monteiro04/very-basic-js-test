3a252d90e210e0de4ed3318af6c49600171be907
felipe-monteiro04 <112021178+felipe-monteiro04@users.noreply.github.com>
2022-10-10 12:42:40 -0300

    Add files via upload

---
+++ js very basic test/index.js
@@ -0,0 +1,14 @@
+var brand, model;
+var idade, peso;
+
+brand = 'Gigabyte';
+model = 'Geforce GTX';
+series = 1070;
+vram = '8gb';
+
+console.log('GPU Brand:', brand)
+console.log('Model:', model);
+console.log('GPU Series:', series);
+console.log('Vram:', vram);
+console.log('**********************************');
+console.log('GPU Full name:', brand, model, series);
